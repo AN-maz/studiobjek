4cc963f8e646852314a0e629b6c9e4827d7a6dff
Andrian <andrianian398@gmail.com>
2024-12-25 13:29:18 +0700

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,9 @@
+function showPopup() {
+    const popup = document.getElementById('popup');
+    popup.classList.remove('hidden');
+}
+
+function closePopup() {
+    const popup = document.getElementById('popup');
+    popup.classList.add('hidden');
+}
